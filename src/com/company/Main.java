7171103abaf478bcfe190b8142074fe622f83a2c
PirotexUA <alex.pavlov.pirotexua@gmail.com>
2017-03-19 21:34:45 +0200

    Zvd1 try to done (Alpha)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Insert x:");
+        double x = in.nextDouble();
+        Zvd1 ZvdObj1 = new Zvd1(x);
     }
 }
---
+++ src/com/company/Zvd1.java
@@ -0,0 +1,39 @@
+package com.company;
+
+/**
+ * Created by Алексей on 19.03.2017.
+ */
+public class Zvd1 {
+    private double x = 0, y = 0, z = 0;
+
+    Zvd1 (double _x){
+    this.x = _x;
+    F1();
+    F2();
+    System.out.println("y = " + y);
+    System.out.println("z = " + z);
+    }
+
+    private void F1 () {
+        if (x < 0) {
+            y = Math.pow(x, 2);
+        }
+        else {
+            y = Math.pow(Math.exp(Math.cos(x)), 1 / 5);
+        }
+    }
+
+    private void F2 () {
+        if (y < 2.0E-01) {
+            z = (5.0E-01 * x - Math.pow(y, 3) + 1);
+        }
+        else
+            if (y > 2.0E-01){
+                z = Math.pow(y, 2) / (x - 0.4);
+            }
+            else
+                if (y == 2.0E-01) {
+                    z = (1 - Math.pow(y, 2)) / Math.pow(3, x);
+                }
+    }
+}
